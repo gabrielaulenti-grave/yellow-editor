e0053d2cbae94c70180070631b98de8567dd95e4
gabrielaulenti-grave <gabriel.aulenti@gmail.com>
2026-09-10 09:35:55 -0400

    Render workspace progress independently of editor tabs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { WorkspaceProgressOverlay } from "./WorkspaceProgressOverlay";
 import "./Editing.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <WorkspaceProgressOverlay />
     <App />
   </React.StrictMode>,
 );
