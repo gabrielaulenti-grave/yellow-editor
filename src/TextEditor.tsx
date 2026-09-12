65cbfffb3ac6136acb5a79b74af996c32b2f0ad6
gabrielaulenti-grave <gabriel.aulenti@gmail.com>
2026-09-12 19:57:06 -0400

    Reuse current dialogue preview when reopening editor

--- src/TextEditor.tsx
+++ src/TextEditor.tsx
@@ -81,7 +81,7 @@ export function TextEditor({
       const next = await invoke<TextDocument>("get_text_document", {
         path: target.path,
         label: target.label,
-        previewText: initialText ?? undefined,
+        previewText: preview ?? undefined,
       });
       setDocument(next);
       setDraft(next.segments.map((segment) => ({ ...segment })));
